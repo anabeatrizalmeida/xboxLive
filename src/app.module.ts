4a9b233ddef8a02deef5968804aebcaa2241661d
Ana Beatriz de Almeida <97922588+anabeatrizalmeida@users.noreply.github.com>
2022-06-30 12:14:57 -0300

    feat(prisma): create module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import {GamesModule} from './games/games.module';
 import {GenresModule} from './genres/genres.module';
 import {ProfilesModule} from './profiles/profiles.module';
 import {UsersModule} from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+
 
 @Module({
-  imports: [GamesModule, GenresModule, ProfilesModule, UsersModule],
+  imports: [GamesModule, GenresModule, ProfilesModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
